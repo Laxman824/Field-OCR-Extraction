5cf1ffa44cadf45c646f4615bbf9dc85bf16e764
K Laxman <71651991+Laxman824@users.noreply.github.com>
2024-11-09 12:02:17 +0530

    8

--- app.py
+++ app.py
@@ -8,7 +8,8 @@
 # from config.field_patterns import FIELD_CATEGORIES
 # Get the absolute path of the current directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
+import os 
+import sys
 # Add the src directory to the Python path
 src_dir = os.path.join(current_dir, 'src')
 print(src_dir)
